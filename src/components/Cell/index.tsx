27ac3be4eecfc9c5d774f3129d4713c3af8b6b6d
liuyib <1656081615@qq.com>
2021-05-19 22:11:59 +0800

    chore: add Cell component

---
+++ src/components/Cell/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './index.scss'
+
+export interface ICell {
+  // 根元素的自定义样式类（值为 active 时，组件呈现为激活变色状态）
+  customClass?: string
+  // 跳转地址
+  link?: string
+  // 左边插槽
+  before?: React.ReactNode
+  // 中间插槽
+  title?: React.ReactNode
+  // 右边插槽
+  after?: React.ReactNode
+}
+
+export const Cell = ({
+  customClass = '',
+  link = '#',
+  before,
+  title,
+  after,
+}: ICell) => {
+  const rootClassName = ['cmp-cell', customClass].filter((v) => v).join(' ')
+
+  return (
+    <a className={rootClassName} href={link}>
+      <div className="cmp-cell-left">{before}</div>
+      <div className="cmp-cell-title">{title}</div>
+      <div className="cmp-cell-right">{after}</div>
+    </a>
+  )
+}
+
+export default Cell
