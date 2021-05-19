5b524ac08c8c434c48d3b9735847d72b833f6cb0
liuyib <1656081615@qq.com>
2021-05-19 11:35:31 +0800

    chore: remove test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import routes from './routes'
 import { RouteWithSubRoutes } from './utils'
 
 const App: React.FC = () => (
   <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/webapp">WebApp</Link>
-        </li>
-        <li>
-          <Link to="/404">404</Link>
-        </li>
-      </ul>
-    </div>
-
     <Switch>
       {routes.map((route, index) => (
         <RouteWithSubRoutes key={index} {...route} />
