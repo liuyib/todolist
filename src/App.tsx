b044aedaa123b498b0c42e5295358a6ad3439393
liuyib <1656081615@qq.com>
2021-05-28 18:58:52 +0800

    chore: rename file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import routes from './routes'
-import { RouteWithSubRoutes } from './utils'
+import { RouteWithSubRoutes } from './utils/router'
 
 const App: React.FC = () => (
   <Router>
