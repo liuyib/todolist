4a7719fdc68d18a9e2422c2ba70f04b9128edbb6
liuyib <1656081615@qq.com>
2021-05-29 17:35:20 +0800

    feat(inbox page): add input element

--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Inbox.scss'
+import Input from '../components/Input'
 
 export const Inbox: React.FC = () => (
-  <>
-    <main className="inbox-center">inbox</main>
-
-    <aside className="inbox-right">inbox-right</aside>
-  </>
+  <main className="inbox">
+    <div className="inbox-header">
+      <Input
+        placeholder="添加任务至“收集箱”，回车即可保存"
+        enter={(value: string) => {
+          console.log(`value`, value)
+        }}
+      ></Input>
+    </div>
+  </main>
 )
 
 export default Inbox
