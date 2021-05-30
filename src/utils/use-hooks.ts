66815080f9901a2a172d066167ef58c10b5484b7
liuyib <1656081615@qq.com>
2021-05-30 23:22:02 +0800

    refactor(utils): make useStore more robust

--- src/utils/use-hooks.ts
+++ src/utils/use-hooks.ts
@@ -2,4 +2,12 @@ import { useContext } from 'react'
 import { StoreContext } from './store-provider'
 import TodoList from '../stores/todo-list'
 
-export const useStore = (): TodoList => useContext(StoreContext)
+export const useStore = (): TodoList => {
+  const storeContext = useContext(StoreContext)
+
+  if (!storeContext) {
+    throw new Error('You are forgot to use StoreProvider.')
+  }
+
+  return storeContext
+}
