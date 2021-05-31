389872e6e0c8aee73e634721b684d469f33b196d
liuyib <1656081615@qq.com>
2021-06-01 00:04:00 +0800

    refactor(cmp): add customClass props to Collapse cmp

--- src/components/Collapse/index.tsx
+++ src/components/Collapse/index.tsx
@@ -3,18 +3,30 @@ import { RightOutlined } from '@ant-design/icons'
 import { useState } from 'react'
 import './index.scss'
 
-interface ICollapseProps {
+export interface ICollapseProps {
+  // 根元素的自定义样式类
+  customClass?: string
+  // 折叠面板标题
   title: string
+  // 左边插槽
   left?: React.ReactNode
+  // 右边插槽
   right?: React.ReactNode
+  // 内容插槽
   content?: React.ReactNode
 }
 
-export const Collapse = ({ title, left, right, content }: ICollapseProps) => {
+export const Collapse = ({
+  customClass,
+  title,
+  left,
+  right,
+  content,
+}: ICollapseProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className="cmp-collapse">
+    <div className={['cmp-collapse', customClass].filter((v) => v).join(' ')}>
       <div className="cmp-collapse-header" onClick={() => setIsOpen(!isOpen)}>
         <div className={`cmp-collapse-header-left${isOpen ? ' active' : ''}`}>
           {left ? left : <RightOutlined />}
